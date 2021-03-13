771729919795481b148baa05fc83356e396b0584
hao <mstoryh@hotmail.com>
2021-03-12 22:04:06 -0500

    Edit

--- Assignment2.java
+++ Assignment2.java
@@ -108,7 +108,7 @@ public class Assignment2 {
 			ps1.setInt(2, flightID);
 			ResultSet rs1 = ps1.executeQuery();
 			rs1.next();
-			capacity = Integer.parseInt(rs1.getString(seatClass));
+			capacity = rs1.getInt(seatClass);
 			
 			
 			// Find how many seats are already occupied for seatClass on flightID
