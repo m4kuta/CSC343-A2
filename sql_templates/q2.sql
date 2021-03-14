d92239b8f1d787133ad09c6a85dda9196938a061
Fan Haolin <fanhaoli@teach.cs.toronto.edu>
2021-03-14 00:30:42 -0500

    Edited query 2

--- sql_templates/q2.sql
+++ sql_templates/q2.sql
@@ -15,16 +15,21 @@ CREATE TABLE q2 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
 drop view if exists Flights cascade;
-drop view if exists FlightBookingInfo cascade;
-drop view if exists RefundableFlights cascade;
-drop view if exists AirlineYearRefunds cascade;
+drop view if exists FlightBookings cascade;
+drop view if exists FlightRefunds cascade;
+-- drop view if exists AirlineYearRefunds cascade;
 
 
 -- Define views for your intermediate steps here:
 create view Flights as
-select F1.id, F1.airline, F1.country as dep_country, F2.country as arv_country, F1.s_dep, F1.s_arv 
+select 
+    F1.id, F1.airline, F1.country as dep_country, F2.country as arv_country, 
+    case 
+        when F1.country = F2.country then true
+        when F1.country != F2.country then false
+    end as intl,
+    F1.s_dep, F1.s_arv 
 from
     (flight join airport on outbound = code) as F1
     join 
@@ -32,12 +37,14 @@ from
     on F1.id = F2.id;
 
 
-create view FlightBookingInfo as
+create view FlightBookings as
 select 
-    Flights.id as flight_id, booking.id as booking_id, airline, name, dep_country, arv_country, seat_class, price, 
+    Flights.id as flight_id, airline, name, 
+    dep_country, arv_country, intl, 
     s_dep, departure.datetime as a_dep, s_arv, arrival.datetime as a_arv, 
     (departure.datetime - s_dep) as dep_delay, 
-    (arrival.datetime - s_arv) as arv_delay 
+    (arrival.datetime - s_arv) as arv_delay,
+    booking.id as booking_id, seat_class, price 
 from 
     Flights
     join airline on (Flights.airline = airline.code)
@@ -46,23 +53,53 @@ from
     join booking on (Flights.id = booking.flight_id);
 
 
-create view RefundableFlights as
+create view FlightRefunds as
 select 
     *,
     case 
-        when dep_country = arv_country and dep_delay >= '05:00:00' then 0.35 * price
-        when dep_country = arv_country and dep_delay >= '10:00:00' then 0.50 * price
-        when dep_country != arv_country and dep_delay >= '08:00:00' then 0.35 * price
-        when dep_country != arv_country and dep_delay >= '12:00:00' then 0.50 * price 
+        when intl = false and  dep_delay < '10:00:00' then 0.35 * price
+        when intl = false and dep_delay >= '10:00:00' then 0.50 * price
+        when intl = true and dep_delay < '12:00:00' then 0.35 * price
+        when intl = true and dep_delay >= '12:00:00' then 0.50 * price 
     end as refund
-from FlightBookingInfo
-where (dep_delay > '05:00:00') and (arv_delay > (dep_delay)/2); 
+from 
+    FlightBookings
+where 
+    ((intl = false and dep_delay > '05:00:00') or 
+    (intl = true and dep_delay > '08:00:00')) 
+    and (arv_delay > (dep_delay)/2); 
 
-create view AirlineYearRefunds as
-select airline, name, extract(year from s_dep) as year, seat_class, sum(refund) as refund
-from RefundableFlights
-group by airline, name, year, seat_class;
+-- select 
+--     *,
+--     case
+--         when dep_country = arv_country then
+--             case 
+--                 when dep_delay >= '05:00:00' and (arv_delay > (dep_delay)/2) then
+--                     case 
+--                         when dep_delay < '10:00:00' then 0.35 * price
+--                         when dep_delay >= '10:00:00' then 0.50 * price
+--                     end
+--                 else 0
+--             end
+--         when dep_country != arv_country then
+--             case
+--                 when dep_delay >= '08:00:00' and (arv_delay > (dep_delay)/2) then
+--                     case 
+--                         when dep_delay < '12:00:00' then 0.35 * price
+--                         when dep_delay >= '12:00:00' then 0.50 * price 
+--                     end
+--                 else 0
+--             end
+--         else 0
+--     end as refund
+-- from FlightBookingInfo; 
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q2
-select * from AirlineYearRefunds;
+select 
+    airline, name, extract(year from s_dep) as year, seat_class, 
+    sum(refund) as refund
+from 
+    FlightRefunds
+group by 
+    airline, name, year, seat_class;
