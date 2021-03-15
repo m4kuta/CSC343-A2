78c5308b369b6db7ab0abcd5a2f84996dfc9f61d
hao <mstoryh@hotmail.com>
2021-03-15 13:28:46 -0400

    Formatted q2, q4, q5

--- misc/tests.sql
+++ misc/tests.sql
@@ -1,4 +1,4 @@
--- query 1
+-- query 2
 select 
     *,
     case
@@ -24,7 +24,111 @@ select
     end as refund
 from FlightBookingInfo; 
 
-Your query that answers the question goes below the "insert into" line:
+
+-- select 
+--     F1.id, F1.airline, F1.country as dep_country, F2.country as arv_country, 
+--     case 
+--         when F1.country = F2.country then true
+--         when F1.country != F2.country then false
+--     end as intl,
+--     F1.s_dep, F1.s_arv 
+-- from
+--     (flight join airport on outbound = code) as F1
+--     join 
+--     (flight join airport on inbound = code) as F2
+--     on F1.id = F2.id;
+
+
+-- select 
+--     Flights.id as flight_id, airline, name, 
+--     dep_country, arv_country, intl, 
+--     s_dep, departure.datetime as a_dep, s_arv, arrival.datetime as a_arv, 
+--     (departure.datetime - s_dep) as dep_delay, 
+--     (arrival.datetime - s_arv) as arv_delay,
+--     booking.id as booking_id, seat_class, price 
+-- from 
+--     Flights
+--     join airline on (Flights.airline = airline.code)
+--     join departure on (Flights.id = departure.flight_id)
+--     join arrival on (Flights.id = arrival.flight_id)
+--     join booking on (Flights.id = booking.flight_id);
+
+
+-- query 4
+-- drop view if exists PlaneCap cascade;
+-- drop view if exists FlightsByPlane cascade;
+-- drop view if exists PercentCapacity cascade;
+
+
+-- create view PlaneCap as
+-- select plane.airline, tail_number, (capacity_economy + capacity_business + capacity_first) as total_cap 
+-- from plane
+-- group by tail_number;
+
+-- create view FlightsByPlane as
+-- select plane, flight.id as flight_id, count(*) as total_passengers
+-- from 
+-- 	flight 
+-- 	right join booking on flight.id = booking.flight_id
+-- 	right join departure on flight.id = departure.flight_id
+-- group by plane, flight.id;
+-- -- Need to include flights that have no bookings and planes that had no flights
+
+-- create view PercentCapacity as
+-- select airline, tail_number, flight_num, total_cap, total_passengers, total_passengers/total_cap::float as percent_cap
+-- from PlaneCap join FlightsByPlane on PlaneCap.tail_number = FlightsByPlane.plane;
+
+
+-- select 
+-- 	airline, tail_number, 
+-- 	count(case when percent_cap < 0.2 then 1 end) as very_low, 
+-- 	count(case when 0.2 <= percent_cap and percent_cap < 0.4 then 1 end) as low, 
+-- 	count(case when 0.4 <= percent_cap and percent_cap < 0.6 then 1 end) as fair, 
+-- 	count(case when 0.6 <= percent_cap and percent_cap < 0.8 then 1 end) as normal, 
+-- 	count(case when 0.8 <= percent_cap then 1 end) as high
+-- from PercentCapacity
+-- group by airline, tail_number, flight_num;
+
+
+-- query 5
+-- Test queries
+
+select * 
+from Hop
+order by n, id;
+
+
+create TEMPORARY view hop1 as
+select 
+	1 as n, id, outbound, inbound, s_dep, s_arv 
+from 
+	flight
+where 
+	extract(year from s_dep) = extract(year from (select day from day)) 
+	and extract(day from s_dep) = extract(day from (select day from day))
+	and outbound = 'YYZ';
+
+
+create TEMPORARY view hop2 as
+select 
+	n + 1 as n, flight.id, flight.outbound, flight.inbound, flight.s_dep, flight.s_arv
+from
+	hop1 cross join Flight
+where 
+	n < (select n from n) and hop1.inbound = Flight.outbound and (Flight.s_dep - hop1.s_arv) <= '24:00:00' and (Flight.s_dep - hop1.s_arv) >= '00:00:00';
+
+
+create TEMPORARY view hop3 as 
+select 
+	n + 1 as n, flight.id, flight.outbound, flight.inbound, flight.s_dep, flight.s_arv
+from
+	hop2 cross join Flight
+where 
+	n < (select n from n) and hop2.inbound = Flight.outbound and (Flight.s_dep - hop2.s_arv) <= '24:00:00' and (Flight.s_dep - hop2.s_arv) >= '00:00:00';
+
+
+select * from hop1 union all select * from hop2 union all select * from hop3;
+
 
 -- bookSeat q1
 select capacity_economy
--- psql/q4.sql
+++ psql/q4.sql
@@ -17,11 +17,6 @@ CREATE TABLE q4 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-
--- drop view if exists PlaneCap cascade;
--- drop view if exists FlightsByPlane cascade;
--- drop view if exists PercentCapacity cascade;
-
 drop view if exists PlaneFullFlight;
 drop view if exists FlightFullBooking;
 drop view if exists PlaneFullBooking;
@@ -29,26 +24,6 @@ drop view if exists PlaneFullDepart;
 drop view if exists PlaneCapacity;
 
 -- Define views for your intermediate steps here:
-
--- create view PlaneCap as
--- select plane.airline, tail_number, (capacity_economy + capacity_business + capacity_first) as total_cap 
--- from plane
--- group by tail_number;
-
--- create view FlightsByPlane as
--- select plane, flight.id as flight_id, count(*) as total_passengers
--- from 
--- 	flight 
--- 	right join booking on flight.id = booking.flight_id
--- 	right join departure on flight.id = departure.flight_id
--- group by plane, flight.id;
--- -- Need to include flights that have no bookings and planes that had no flights
-
--- create view PercentCapacity as
--- select airline, tail_number, flight_num, total_cap, total_passengers, total_passengers/total_cap::float as percent_cap
--- from PlaneCap join FlightsByPlane on PlaneCap.tail_number = FlightsByPlane.plane;
-
-
 create view PlaneFullFlight as
 select
 	flight.id as flight_id, flight.airline, plane.tail_number, 
@@ -59,7 +34,8 @@ from
 
 create view FlightFullBooking as
 select 
-	flight.id as flight_id, flight.airline, flight.plane, booking.id as booking_id
+	flight.id as flight_id, flight.airline, flight.plane, 
+	booking.id as booking_id
 from 
 	flight full join booking on flight.id = booking.flight_id;
 
@@ -68,14 +44,17 @@ create view PlaneFullBooking as
 select 
 	PF.flight_id, Pf.airline, PF.tail_number, PF.capacity, FB.booking_id
 from 
-	PlaneFullFlight PF full join FlightFullBooking as FB on PF.flight_id = FB.flight_id;
+	PlaneFullFlight PF full join FlightFullBooking as FB 
+	on PF.flight_id = FB.flight_id;
 
 
 create view PlaneFullDepart as
 select 
-	PB.flight_id, PB.airline, PB.tail_number, PB.capacity, departure.datetime, PB.booking_id
+	PB.flight_id, PB.airline, PB.tail_number, PB.capacity, departure.datetime, 
+	PB.booking_id
 from 
-	PlaneFullBooking PB full join departure on PB.flight_id = departure.flight_id
+	PlaneFullBooking PB full join departure 
+	on PB.flight_id = departure.flight_id
 order by 
 	PB.flight_id;
 
@@ -95,16 +74,6 @@ order by
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q4
--- select 
--- 	airline, tail_number, 
--- 	count(case when percent_cap < 0.2 then 1 end) as very_low, 
--- 	count(case when 0.2 <= percent_cap and percent_cap < 0.4 then 1 end) as low, 
--- 	count(case when 0.4 <= percent_cap and percent_cap < 0.6 then 1 end) as fair, 
--- 	count(case when 0.6 <= percent_cap and percent_cap < 0.8 then 1 end) as normal, 
--- 	count(case when 0.8 <= percent_cap then 1 end) as high
--- from PercentCapacity
--- group by airline, tail_number, flight_num;
-
 select 
 	airline, tail_number, 
 	count(case when 0.0 <= percent_cap and percent_cap < 0.2 then 1 end) as very_low, 
